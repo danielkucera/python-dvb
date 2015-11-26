7dd8002c3d95f5ac1c0345d9415112de6dae3945
root <root@iptvmaster2.vnet.sk>
2015-11-26 09:01:32 +0100

    change output formatting

--- checkcc.py
+++ checkcc.py
@@ -47,10 +47,10 @@ def main():
 	print "Have stream"
 
     if lastTime + 1 < time.time():
-	print (round(float(scrambledCnt)/(pktCnt + 1)*100)), "% scrambled",
-	print CCerrors, "CC errors",
-	print (pktCnt*188*8)/1024, "Kbps",
-	print counter.keys(),
+	print (round(float(scrambledCnt)/(pktCnt + 1)*100)), "%crypt,",
+	print CCerrors, "CCerr,",
+	print (pktCnt*188*8)/1024, "Kbps,",
+	print "PIDs:",counter.keys(),
 	sys.stdout.flush()
 	print "\r",
 	pktCnt = 0
